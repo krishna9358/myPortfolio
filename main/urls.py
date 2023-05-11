2c2baf72274cc4045845abdc847ca3fdce64f9c8
Krishna Mohan <krishanmohank974@gmail.com>
2023-05-12 02:06:14 +0530

    made a new path blog_view_article and also

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import blog_view, blog_detail_view
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("blog_view/", views.blog_view, name="blog_view")
-]
+    # path("blog_view/", views.blog_view, name="blog_view")
+    path("blog_view/", blog_view.as_view(), name="blog_view"),
+    path('article/<int:pk>',  blog_detail_view.as_view(), name="article") 
+] 
--- main/views.py
+++ main/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from  .models import Post
 # Create your views here.
 def index(request):
     return render(request, "main/index.html")
 
-def blog_view(request):
-    return render(request, "main/blog_view.html", {})
+# def blog_view(request):
+#     return render(request, "main/blog_view.html", {})
+
+class blog_view(ListView):
+    model = Post
+    template_name = 'main/blog_view.html'
+
+class blog_detail_view(DetailView):
+    model = Post
+    template_name = 'main/article.html'
